054671301f215e6b6182dbedccc3924d225ebf3a
ShindQQ <denis110402@gmail.com>
2023-09-11 16:47:15 +0300

    Add project files.

---
+++ PZA1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+
+var dateTimeNow = DateTime.UtcNow;
+var highLevel = false;
+var lowLevel = false;
+var pump = false;
+
+while (true)
+{
+    Console.WriteLine(
+        """
+        Enter option for your level:
+        1. Set highLevel to !highLevel (default false)
+        2. Set lowLevel to !lowLevel (default false) and highLevel to false
+        3. Skip
+        """);
+    var option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            highLevel = !highLevel;
+            dateTimeNow = DateTime.UtcNow;
+            break;
+        case 2:
+            lowLevel = !lowLevel;
+            highLevel = false;
+            break;
+        default: break;
+    };
+
+    if (highLevel)
+    {
+        var timeNow = DateTime.UtcNow;
+        var timeDifferenceInSeconds = (timeNow - dateTimeNow).TotalSeconds;
+        Console.WriteLine($"Elapsed time: {timeDifferenceInSeconds}");
+
+        if (timeDifferenceInSeconds >= 3)
+        {
+            pump = true;
+            dateTimeNow = timeNow;
+            Console.WriteLine("Pump has been activated");
+        }
+    }
+
+    if (lowLevel && pump)
+    {
+        pump = false;
+        lowLevel = false;
+        highLevel = false;
+        Console.WriteLine("Pump has been deactivated");
+    }
+}
